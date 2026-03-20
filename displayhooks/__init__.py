969129d1a596e118767ee9501f5b07819b9bc9a0
Евгений Блинов <pomponchik@Mac-mini-Evgenij.local>
2026-03-20 19:56:02 +0300

    Remove .ruff.toml and refactor imports to use consistent typing and formatting

--- displayhooks/__init__.py
+++ displayhooks/__init__.py
@@ -1,3 +1,7 @@
-from displayhooks.converter import converted_displayhook as converted_displayhook  # noqa: F401
-from displayhooks.autorestore import autorestore_displayhook as autorestore_displayhook  # noqa: F401
-from displayhooks.not_display import not_display as not_display  # noqa: F401
+from displayhooks.autorestore import (
+    autorestore_displayhook as autorestore_displayhook,
+)
+from displayhooks.converter import (
+    converted_displayhook as converted_displayhook,
+)
+from displayhooks.not_display import not_display as not_display
